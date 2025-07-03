d45dcf6ab2a557764b8caa7dd0bc366f1546eaa7
LeinneSW <nno88551@naver.com>
2025-07-03 15:15:30 +0900

    game : 홈 이동경고를 modal로 변경

--- public/game/js/logic.js
+++ public/game/js/logic.js
@@ -2,6 +2,7 @@ import {ChzzkClient} from "https://cdn.skypack.dev/chzzk"
 import {addMessage, clearMessageList, convertColorCode, updateQuiz, updateRankGraph} from "./ui.js";
 import {getChannelId} from '../../util/util.js';
 import {getGameState, getScores, resetGameState, resetScores, setGameState, setScores} from "./data.js";
+import {showConfirmModal} from "./modal.js";
 
 const scores = getScores()
 const gameState = getGameState()
@@ -102,11 +103,11 @@ window.addEventListener('load', () => {
         return;
     }
     document.getElementById('home-btn').onclick = () => {
-        if(confirm('정말 진행중이던 게임을 종료하고 홈 화면으로 돌아가시겠습니까?')){
+        showConfirmModal('게임 종료', '정말 진행중이던 게임을 종료하고 홈 화면으로 돌아가시겠습니까?', () => {
             resetScores()
             resetGameState()
             location.href = '/home/'
-        }
+        })
     };
     document.getElementById('next-btn').onclick = nextRound;
     if(gameState.solved){
